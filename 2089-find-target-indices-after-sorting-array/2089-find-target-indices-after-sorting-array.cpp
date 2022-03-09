96e00c97ecbfb2e5aa4b2b1c0bcc321178c3583a
Manish Kumar Shahi <54751210+ManishKrShahi@users.noreply.github.com>
2022-03-09 15:54:03 +0530

    Time: 0 ms (100.00%), Space: 11.6 MB (61.76%) - LeetHub

---
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        vector<int> res;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target) res.push_back(i);
+            if(nums[i]>target) break;
+        }
+        return res;
+    }
+};
