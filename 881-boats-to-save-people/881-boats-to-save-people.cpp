e0ab9143f6377ac2cbb1ddc2b35820f729583b9b
Manish Kumar Shahi <54751210+ManishKrShahi@users.noreply.github.com>
2022-03-24 22:14:02 +0530

    Time: 76 ms (87.76%), Space: 41.9 MB (91.53%) - LeetHub

---
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int cnt = 0;
+        sort(people.begin(), people.end());
+        
+        int l = 0;
+        int r = people.size() - 1;
+        
+        while(l <= r){
+            int sum = people[l] + people[r];
+            if(sum <= limit){
+                cnt++;
+                l++;
+                r--;
+            }
+            else{
+                cnt++;
+                r--;
+            }
+        }
+        return cnt;
+    }
+    
+};
