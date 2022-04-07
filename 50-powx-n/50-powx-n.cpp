dad71fc1aa9151c496db96a058a5252fd0003637
Manish Kumar Shahi <54751210+ManishKrShahi@users.noreply.github.com>
2022-04-07 12:17:26 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.26%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double res = 1;
+        while (n) {
+            if (n % 2) {
+                if(n > 0)
+                    res=res * x ;
+                else
+                    res=res / x;
+            }
+            x = x*x;
+            n=n/2;
+        }
+        return res;
+    }
+};
