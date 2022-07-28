edcbf1782f6ee44c778b4a1fbdcd52e8688d89f3
Manish Kumar Shahi <54751210+ManishKrShahi@users.noreply.github.com>
2022-07-28 23:59:00 +0530

    Time: 10 ms (34.41%), Space: 6 MB (25.76%) - LeetHub

--- 390-elimination-game/390-elimination-game.cpp
+++ 390-elimination-game/390-elimination-game.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     int lastRemaining(int n) {
-        if (n == 1) return 1;
-        if (n <= 4) return 2;
-        if (n % 2 != 0) n -= 1;
-        if (n % 4 != 0) return 4 * lastRemaining(n/4);
-        return 4 * lastRemaining(n/4) - 2;
-
-    }
+    if(n==1)    return 1;
+    return 2*((n/2)-lastRemaining(n/2)+1);
+}
 };
