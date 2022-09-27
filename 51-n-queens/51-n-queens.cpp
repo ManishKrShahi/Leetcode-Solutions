270b630085d4765c777a2e66f703e80de89e3117
Manish Kumar Shahi <54751210+ManishKrShahi@users.noreply.github.com>
2022-09-27 00:54:40 -0700

    Time: 3 ms (96.81%), Space: 7.2 MB (85.13%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void solve(int col,vector<string> &bd,vector<vector<string>> &res,int n, vector<int> &lr,vector<int> &ld,vector<int> &ud){
+        if(col==n){
+            res.push_back(bd);
+            return;
+        }
+        for(int row=0;row<n;row++){
+            if(lr[row]==0 && ld[row+col]==0 && ud[n-1+col-row]==0){
+                bd[row][col]='Q';
+                lr[row]=1;
+                ld[row+col]=1;
+                ud[n-1+col-row]=1;
+                solve(col+1,bd,res,n,lr,ld,ud);
+                bd[row][col]='.';
+                lr[row]=0;
+                ld[row+col]=0;
+                ud[n-1+col-row]=0;
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> res;
+        vector<string> bd(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            bd[i]=s;
+        }
+        vector<int> lr(n,0),ud(2*n-1,0),ld(2*n-1,0);
+        solve(0,bd,res,n,lr,ld,ud);
+        return res;
+    }
+};
