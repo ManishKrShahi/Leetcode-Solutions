7f47d7d06f4d042902fb4ccfc97d12de98bfca99
Manish Kumar Shahi <54751210+ManishKrShahi@users.noreply.github.com>
2022-10-04 13:24:01 -0700

    Time: 4 ms (57.45%), Space: 5.9 MB (38.67%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int dp[31];
+    /*int dp[31];
     int helper(int n){
         dp[0]=0;
         dp[1]=1;
@@ -8,9 +8,17 @@ public:
             dp[i]=dp[i-1]+dp[i-2];
         }
         return dp[n];
-    }
+    }*/
     int fib(int n) {
-       // memset(dp,-1,sizeof(dp));
-        return helper(n);
+        if(n==0)
+            return 0;
+        int prev=1;
+        int prev2=0;
+        for(int i=2;i<=n;i++){
+            int curr=prev+prev2;
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
     }
 };
